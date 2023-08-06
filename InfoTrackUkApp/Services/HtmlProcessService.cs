6beccb262fdf02a0b6498b87b48409b9959a2444
Nabeel Ahsan <ahsanmuhammadnabeel@hotmail.com>
2023-08-07 00:54:31 +0100

    edits

--- InfoTrackUkApp/Services/HtmlProcessService.cs
+++ InfoTrackUkApp/Services/HtmlProcessService.cs
@@ -37,30 +37,5 @@ namespace InfoTrackUkApp.Services
             }
             
         }
-        
-        //public string GetRanks(string searchResults, string url)
-        //{
-        //    string pattern = $@"\b{Regex.Escape(url)}\b";
-        //    MatchCollection matches = Regex.Matches(searchResults, pattern, RegexOptions.IgnoreCase);
-
-        //    string ranks = "";
-        //    int matchCount = matches.Count;
-        //    if (matchCount > 0)
-        //    {
-        //        for (int i = 0; i < matchCount; i++)
-        //        {
-        //            ranks += matches[i].Index.ToString();
-        //            if (i < matchCount - 1)
-        //            {
-        //                ranks += ",";
-        //            }
-        //        }
-        //        return ranks;
-        //    }
-        //    else
-        //    {
-        //        return "0"; // URL not found in results
-        //    }
-        //}
     }
 }
